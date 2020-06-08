90c7d2d4d59b3df227c05e65952c594b17338ed9
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-06-08 14:48:38 -0600

    Update Cilindro_R.m

--- Cilindro_R.m
+++ Cilindro_R.m
@@ -6,8 +6,30 @@
 %% GLOBAL
 
 global R
-global E
+global E_0
 
 %% Computar los valores
-R = 1000;
-E = 30;
+R = 10;
+E_0 = 5;
+
+figure(1)
+x1 = R:50;
+y1 = 0:2*pi;
+[x, y] = meshgrid(x1, y1);
+
+F= (x>R).*(-E_0.*x.*cos(y).*(1-(R^2./x.^2)));
+
+
+meshc(x,y,F);
+colormap (hsv)
+colorbar; 
+grid off
+
+xticks([R 50])
+%%xticklabels({'0', '500', '1000'})
+xlabel('r')
+%%yticks([0 (3*pi)/4 pi])
+%%yticklabels({'0', '25', '50'})
+ylabel('theta')
+zlabel('V(r,theta)');
+title('Potencial en 3D');
