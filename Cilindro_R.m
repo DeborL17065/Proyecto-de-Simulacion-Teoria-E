43949a02f906099135d05ae06521c162735e2e83
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-06-08 14:18:40 -0600

    Create Cilindro_R.m

---
+++ Cilindro_R.m
@@ -0,0 +1,13 @@
+%% Universidad del Valle de Guatemala
+% Departamento de Ingenieria
+% Teoría Electromagnética 1       Secci?n:    10
+% Debora López               Carne:   17065
+
+%% GLOBAL
+
+global R
+global E
+
+%% Computar los valores
+R = 1000;
+E = 30;
