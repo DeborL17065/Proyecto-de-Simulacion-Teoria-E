b61aacbd0de08f6061445ca47781e6a1570c233c
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-06-08 21:59:12 -0600

    Update Esfera_ab.m

--- Esfera_ab.m
+++ Esfera_ab.m
@@ -25,16 +25,11 @@ y1 = 0:pi;
 [x, y] = meshgrid(x1, y1);
 F = (x<a).*(((k1/2).*(1-(x/a).*cos(y))));
 
-meshc(x,y,F);
+meshc(F);
 colormap (hsv)
 colorbar; 
 grid off
 
-
-hold on
-U= gradient(F);
-quiver3(x,y,U,U)
-hold off
 
 xticks([0 a ])
 %%xticklabels({'0', '500', '1000'})
@@ -46,21 +41,26 @@ zlabel('V(r,theta)');
 title('Potencial en 3D');
 
 figure(2)
+x1 = 0:a;
+y1 = 0:pi;
+[x, y] = meshgrid(x1, y1);
+F = (x<a).*(((k1/2).*(1-(x/a).*cos(y))));
+
+U= gradient(F);
+quiver3(x,y,U,U)
+
+
+figure(3)
 x1 = a:b;
 y1 = 0:pi;
 [x, y] = meshgrid(x1, y1);
 F = (a<x<b).*(((k1/2)*(a-((x).*cos(y))))-((k*b.*x)/e)+ ((2*k*(x.^2))/(5*e*b)).*((3/2)*((cos(y)).^2)-(1/2)));
 
-meshc(x,y,F);
+meshc(F);
 colormap (hsv)
 colorbar; 
 grid off
 
-
-hold on
-U= gradient(F);
-quiver3(x,y,U,U)
-hold off
 
 xticks([ a  b ])
 %%xticklabels({'0', '500', '1000'})
@@ -71,21 +71,28 @@ ylabel('theta')
 zlabel('V(r,theta)');
 title('Potencial en 3D');
 
-figure(3)
+figure(4)
+x1 = a:b;
+y1 = 0:pi;
+[x, y] = meshgrid(x1, y1);
+F = (a<x<b).*(((k1/2)*(a-((x).*cos(y))))-((k*b.*x)/e)+ ((2*k*(x.^2))/(5*e*b)).*((3/2)*((cos(y)).^2)-(1/2)));
+
+
+U= gradient(F);
+quiver3(x,y,U,U)
+
+
+figure(5)
 x1 = b:50;
 y1 = 0:pi;
 [x, y] = meshgrid(x1, y1);
 F = (x>=b).*(((k1/2)*(a-((x).*cos(y))))-((k*b^2)./e*x)+ ((2*k*(b^4))./(5*e*x.^3)).*((3/2)*((cos(y)).^2)-(1/2)));
 
-meshc(x,y,F);
+meshc(F);
 colormap (hsv)
 colorbar; 
 grid off
 
-hold on
-U= gradient(F);
-quiver3(x,y,U,U)
-hold off
 
 xticks([ b 50])
 %%xticklabels({'0', '500', '1000'})
@@ -95,4 +102,15 @@ xlabel('r')
 ylabel('theta')
 zlabel('V(r,theta)');
 title('Potencial en 3D');
+
+figure(6)
+x1 = b:50;
+y1 = 0:pi;
+[x, y] = meshgrid(x1, y1);
+F = (x>=b).*(((k1/2)*(a-((x).*cos(y))))-((k*b^2)./e*x)+ ((2*k*(b^4))./(5*e*x.^3)).*((3/2)*((cos(y)).^2)-(1/2)));
+
+
+
+U= gradient(F);
+quiver3(x,y,U,U)
 
