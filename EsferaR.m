51916af143c09ddd169633f7205e89c03969e468
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-06-08 21:59:17 -0600

    Update EsferaR.m

--- EsferaR.m
+++ EsferaR.m
@@ -13,27 +13,23 @@ R = 10;
 e = 1;
 
 
-VOLTAJE_ADENTRO =  @(x,y)(-(((3*x)/(2*e)).*cos(y))+((6*(x.^3))/(7*e*(R^2))).*((5/2).*((cos(y)).^3)-((3/2).*cos(y))));
-VOLTAJE_AFUERA =  @(x,y)(-(((3*(R^3))./(2*e*(x.^2))).*cos(y))+((6*(R^5))./(7*e*(x.^4))).*((5/2).*((cos(y)).^3)-((3/2).*cos(y))));
-
 figure(1)
 x1 = 0:50;
-y1 = 0:pi;
+y1 = 0:2*pi/100:pi;
 [x, y] = meshgrid(x1, y1);
 F = (x<R).*((((3*x)/(2*e)).*cos(y))-((6*(x.^3))/(7*e*(R^2))).*((5/2).*((cos(y)).^3)-((3/2).*cos(y))))+ (x>=R).*((((3*(R^3))./(2*e*(x.^2))).*cos(y))-((6*(R^5))./(7*e*(x.^4))).*((5/2).*((cos(y)).^3)-((3/2).*cos(y))));
-
-meshc(x,y,F);
+mesh(F);
 colormap (hsv)
 colorbar; 
-grid off
+%%grid off
 
 
 xticks([0 R 50])
 %%xticklabels({'0', '500', '1000'})
-xlabel('r')
-%%yticks([0 (3*pi)/4 pi])
+%%xlabel('r')
+%%yticks([0  pi])
 %%yticklabels({'0', '25', '50'})
-ylabel('theta')
+%%ylabel('theta')
 zlabel('V(r,theta)');
 title('Potencial en 3D');
 
